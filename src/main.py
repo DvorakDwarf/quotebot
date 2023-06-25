5f9942c43061b625b09ec52c4baa3fc494508370
HistidineDwarf <dvorakdwarf@gmail.com>
2023-06-25 11:35:04 -0700

    Connected to MySQL

--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+from mysql.connector import connect, Error
 
 #Secrets
 load_dotenv()
 
 PREFIX = os.getenv("PREFIX")
 TOKEN = os.getenv("TOKEN")
+MAX_QUOTE_LENGTH = int(os.getenv("MAX_QUOTE_LENGTH"))
+SQL_USERNAME = os.getenv("SQL_USERNAME")
+SQL_PASSWORD = os.getenv("SQL_PASSWORD")
+DATABASE = os.getenv("DATABASE")
 
 #Bot setup
 intents = discord.Intents.all()
@@ -17,16 +22,37 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 async def on_ready():
     print("Bot started")
 
-@bot.command(name="addquote")
-async def addquote(ctx, quote = None):
-    msg = ctx.message
+    #Connecting to database
+    try:
+        with connect(
+            host="localhost",
+            user=SQL_USERNAME,
+            password=SQL_PASSWORD,
+            database=DATABASE,
+        ) as connection:
+            print(connection)
+    except Error as e:
+        print(e)
 
-    if quote == None:
-        await msg.channel.send("You didn't add a quote dumbass")
+
+
+@bot.command(name="addquote")
+async def addquote(ctx, author = None, quote = None):
+    #32 is max discord username character limit
+    if author == None or len(author) > 32:
+        await ctx.send("Invalid author bozo")
         return
-        
+
+    if quote == None or quote > MAX_QUOTE_LENGTH:
+        await ctx.send("Invalid quote dumbass")
+        return
+
 
     print(quote)
+
+@bot.command(name="random")
+async def random(ctx):
+    pass
     
 bot.run(TOKEN)
 
