0fa12a989d49a3befee2e354c8c4ba42987a041e
HistidineDwarf <dvorakdwarf@gmail.com>
2023-06-26 14:56:16 -0700

    Making random work

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
+#TODO
+#Consider slash commands
+#Find other way to insert author
+#Possibly date
+
 import discord
 from discord.ext import commands
 import os
+import random
 from dotenv import load_dotenv
 from mysql.connector import connect, Error
 
@@ -18,41 +24,56 @@ DATABASE = os.getenv("DATABASE")
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+#Connecting to database
+try:
+    connection = connect(
+        host="localhost",
+        user=SQL_USERNAME,
+        password=SQL_PASSWORD,
+        database=DATABASE,
+    )
+except Error as e:
+    print(e)
+
+
 @bot.event
 async def on_ready():
     print("Bot started")
 
-    #Connecting to database
-    try:
-        with connect(
-            host="localhost",
-            user=SQL_USERNAME,
-            password=SQL_PASSWORD,
-            database=DATABASE,
-        ) as connection:
-            print(connection)
-    except Error as e:
-        print(e)
-
-
 
 @bot.command(name="addquote")
-async def addquote(ctx, author = None, quote = None):
-    #32 is max discord username character limit
-    if author == None or len(author) > 32:
-        await ctx.send("Invalid author bozo")
-        return
+async def addquote(ctx):
+    msg = ctx.message.content[10:]
 
-    if quote == None or quote > MAX_QUOTE_LENGTH:
+    # #32 is max discord username character limit
+    # if author == None or len(author) > 32:
+    #     await ctx.send("Invalid author bozo")
+    #     return
+
+    if len(msg) > MAX_QUOTE_LENGTH:
         await ctx.send("Invalid quote dumbass")
         return
 
-
-    print(quote)
+    # print(author)
+    #JANK, still works
+    print(msg)
+    insert_query = f"INSERT INTO quotes (quote) VALUES ('{msg}')"
+    with connection.cursor() as cursor:
+        cursor.execute(insert_query)
+        connection.commit()
+        cursor.execute("SELECT * FROM quotes")
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
 
 @bot.command(name="random")
-async def random(ctx):
-    pass
+async def pick_random(ctx):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT quote, author FROM quotes")
+        result = cursor.fetchall()
+        print(result)
+        print(type(result))
+        # await ctx.send(random.choice(result)[0] +  " - " )
     
 bot.run(TOKEN)
 
